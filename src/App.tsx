a14d835104541688019bda0ea89ca4cfa0465d04
Michael Farquhar <michaelfarquhar4@gmail.com>
2024-10-15 18:51:25 -0400

    Install Material UI and test basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import {
+  Alert,
+  Button,
+  Card,
+  CardContent,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+
 function App() {
-  return <h1>App</h1>;
+  const [input, setInput] = useState("");
+  const [wasClicked, setWasClicked] = useState(false);
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardContent>
+        <Typography variant="h5">Enter Username:</Typography>
+        <Stack direction="row" spacing={2}>
+          <TextField
+            label="Size"
+            id="outlined-size-small"
+            value={input}
+            onChange={(el) => setInput(el.target.value)}
+            size="small"
+          />
+          <Button variant="contained" onClick={() => setWasClicked(true)}>
+            Submit
+          </Button>
+        </Stack>
+        {wasClicked && <Alert severity="success">Button clicked!</Alert>}
+      </CardContent>
+    </Card>
+  );
 }
 
 export default App;
